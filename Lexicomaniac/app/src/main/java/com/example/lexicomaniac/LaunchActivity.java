1199be5a00e58fe2d4d2183340ba3e138072485f
tejaskb04 <tejaskb@gmail.com>
2017-09-02 19:52:30 -0700

    Setup Authentication

--- Lexicomaniac/app/src/main/java/com/example/lexicomaniac/LaunchActivity.java
+++ Lexicomaniac/app/src/main/java/com/example/lexicomaniac/LaunchActivity.java
@@ -1,13 +1,40 @@
 package com.example.lexicomaniac;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LaunchActivity extends AppCompatActivity {
+    private Button dashboardBtn;
+    private Button switchUserBtn;
+    private Button newUserBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+        dashboardBtn = (Button) findViewById(R.id.dashboard);
+        switchUserBtn = (Button) findViewById(R.id.switchUser);
+        newUserBtn = (Button) findViewById(R.id.register);
+        dashboardBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LaunchActivity.this, DashboardActivity.class));
+            }
+        });
+        switchUserBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LaunchActivity.this, LoginActivity.class));
+            }
+        });
+        newUserBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LaunchActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
---
+++ Lexicomaniac/app/src/main/java/com/example/lexicomaniac/User.java
@@ -0,0 +1,21 @@
+package com.example.lexicomaniac;
+
+public class User {
+    private String email;
+
+    public User() {
+
+    }
+
+    public User(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String s) {
+        this.email = s;
+    }
+}
