a730220794384051cf699f2dda46947ebed6ccb8
tejaskb04 <tejaskb@gmail.com>
2017-09-02 22:35:47 -0700

    Implemented Navigation Toolbar

--- Lexicomaniac/app/src/main/java/com/example/lexicomaniac/DashboardActivity.java
+++ Lexicomaniac/app/src/main/java/com/example/lexicomaniac/DashboardActivity.java
@@ -1,13 +1,40 @@
 package com.example.lexicomaniac;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class DashboardActivity extends AppCompatActivity {
-
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView
+                .OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_dashboard: {
+                        return true;
+                    }
+                    case R.id.navigation_notifications: {
+                        return true;
+                    }
+                    case R.id.navigation_search: {
+                        return true;
+                    }
+                    case R.id.navigation_settings: {
+                        return true;
+                    }
+                    default: {
+                        return false;
+                    }
+                }
+            }
+        });
     }
 }
